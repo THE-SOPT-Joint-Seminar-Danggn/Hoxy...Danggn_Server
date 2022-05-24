cb314a1c202303bb162d2b01971e4d590649017f
jinsu4755 <parkjinsu4755@gmail.com>
2022-05-25 05:33:12 +0900

    feat: create Product Infomation interface
    - add product item infomation interface
    - fix seperate ProductInfo in UserInfo

--- src/interface/UserInfo.ts
+++ src/interface/UserInfo.ts
@@ -1,17 +1,4 @@
-export default interface ProductInfo{
-    user: UserInfo,
-    title: string,
-    category: string,
-    price: number,
-    contents: string,
-    view: number,
-    isPriceSuggestion: boolean,
-    isLiked: boolean,
-    onSale: number,
-    createAt: Date
-}
-
-interface UserInfo {
+export default interface UserInfo {
     profile: string,
     name: string,
     region: string

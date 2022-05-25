5d4f19c595564d2de25124618c97a15085e6ece4
jinsu4755 <parkjinsu4755@gmail.com>
2022-05-25 09:52:20 +0900

    feat: create ProductSchema.ts
    - add UserSchema
    - add ProductSchema

--- src/schema/Product.ts
+++ src/schema/Product.ts
@@ -1,21 +1,29 @@
 import mongoose    from "mongoose";
 import ProductInfo from "../interface/ProductInfo";
 
-const ProductSchema = new mongoose.Schema(
+
+const UserSchema = new mongoose.Schema(
+    {
+        profile: {
+            type: String,
+            default: "https://scontent-ssn1-1.xx.fbcdn.net/v/t39.30808-6/274554211_7058140590927107_9076750575401386959_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=yRBiAOFUvO8AX998_s6&_nc_ht=scontent-ssn1-1.xx&oh=00_AT-g67pKCk_7v55g66vWHn6dJw7DcJn_OSTnOiR_Xl0dxA&oe=6291C318"
+        },
+        name: {
+            type: String,
+            default: "30th-sopt"
+        },
+        region: {
+            type: String,
+            default: "개봉동"
+        }
+    }
+)
+
+const Product = new mongoose.Schema(
     {
         user: {
-            profile: {
-                type: String,
-                default: "https://scontent-ssn1-1.xx.fbcdn.net/v/t39.30808-6/274554211_7058140590927107_9076750575401386959_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=yRBiAOFUvO8AX998_s6&_nc_ht=scontent-ssn1-1.xx&oh=00_AT-g67pKCk_7v55g66vWHn6dJw7DcJn_OSTnOiR_Xl0dxA&oe=6291C318"
-            },
-            name: {
-                type: String,
-                default: "30th-sopt"
-            },
-            region: {
-                type: String,
-                default: "개봉동"
-            }
+            type: UserSchema,
+            default: {}
         },
         title: {
             type: String,
@@ -29,7 +37,7 @@ const ProductSchema = new mongoose.Schema(
             type: Number,
             required: true
         },
-        content: {
+        contents: {
             type: String,
             required: true
         },
@@ -56,4 +64,4 @@ const ProductSchema = new mongoose.Schema(
     }
 );
 
-export default mongoose.model<ProductInfo & mongoose.Document>("Product", ProductSchema)
+export default mongoose.model<ProductInfo & mongoose.Document>("Product", Product)

85b2d1ae7a7458c33039726a59ba21fcde387d2a
김동재 <87058086+ehdwoKIM@users.noreply.github.com>
2022-06-01 03:53:43 +0900

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
-//router index file
-import { Router } from "express";
+import { Router }           from 'express';
+import AddProductController from "../controllers/AddProductController";
+import CreateProductDTO     from "../dto/add/CreateProductDTO";
+import { validateBody }     from "../middlewares/ValidateBody";
 import { ProductController } from "../controllers";
+import DetailController from "../controllers/DetailController";
 
 const router = Router();
 
+const addProductController = new AddProductController();
+router.post('/feed', validateBody(CreateProductDTO), addProductController.postSaveProduct)
 router.get("/feed", ProductController.getAllProducts);
+router.get("/feed_page", ProductController.getAllProductsWithPagging);
 router.put("/feed/like/:productId", ProductController.updateLike);
+router.get('/feed/:productId', DetailController.getDetail);
 
-export default router;

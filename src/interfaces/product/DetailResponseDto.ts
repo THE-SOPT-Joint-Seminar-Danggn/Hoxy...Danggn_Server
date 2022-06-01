7ce90fd282e4e8b67549b486d580dd8e4f4e3c03
김동재 <87058086+ehdwoKIM@users.noreply.github.com>
2022-06-01 16:46:43 +0900

    Update DetailResponseDto.ts

--- src/interfaces/product/DetailResponseDto.ts
+++ src/interfaces/product/DetailResponseDto.ts
@@ -1,11 +1,13 @@
+import { ProductInfo } from "../ProductInfo"
+
 export interface DetailResponseDto {
-    onSale: number,
-    title: string,
-    category: string,
-    createAt: Date,
-    contents: string,
-    view: number,
-    price: number,
-    isPriceSuggestion: boolean,
-    isLiked: boolean
-}
+    onSale: ProductInfo[],
+    title: ProductInfo[],
+    category: ProductInfo[],
+    createAt: ProductInfo[],
+    contents: ProductInfo[],
+    view: ProductInfo[],
+    price: ProductInfo[],
+    isPriceSuggestion: ProductInfo[],
+    isLiked: ProductInfo[]
+} 

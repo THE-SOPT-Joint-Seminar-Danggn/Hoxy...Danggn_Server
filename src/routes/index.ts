9c91794db27dd6cdf3513ecaaf64bc26fb5ad350
김동재 <87058086+ehdwoKIM@users.noreply.github.com>
2022-06-02 19:34:08 +0900

    feat(detail-stat): 판매 상태 변경 기능 추가 (#6)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,5 +14,7 @@ router.get("/feed", ProductController.getAllProducts);
 router.get("/feed_page", ProductController.getAllProductsWithPagging);
 router.put("/feed/like/:productId", ProductController.updateLike);
 router.get('/feed/:productId', DetailController.getDetail);
+router.put("/feed/on-sale", DetailController.updateStat);
+
 
 export default router;
